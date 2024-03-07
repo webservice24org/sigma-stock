4ce31afae9268adab0ae361acb984c74250ada04
webservice24org <webservice24.org@gmail.com>
2024-03-07 10:16:02 +0600

    Sigma Chemical Stock

---
+++ app/Models/CustomerCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerCategory extends Model
+{
+    use HasFactory;
+}
